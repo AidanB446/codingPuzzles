54a1f85f5805d62e14e15665ee50f0c9a4d4a4e8
Aidan Bruner <aidanbruner789@gmail.com>
2024-12-02 20:12:42 -0500

    adventofcode day2

--- adventOfCode/2024/day2/part1/index.js
+++ adventOfCode/2024/day2/part1/index.js
@@ -11,12 +11,67 @@ function solution() {
 
 	for (let i = 0; i < data.length; i++) {
 		const reportList = data[i];
-		const reports = reportList.split(" ");
 		
-		for (let j = 0;  j<reports.length; j++) {
+		console.log(reportList);
+
+		if (reportList.length < 1 ) {
+			continue;
+		}
+
+		// then we know its increasing
+		if (parseInt(reportList[0]) < parseInt(reportList[1])) {
+			
+			// valid until proven guilty
+			let validity = true;
+
+			for (let j = 0; j < reportList.length - 1; j++) {
+				if (parseInt(reportList[j]) > parseInt(reportList[j+1])) {
+					validity = false;
+					console.log("invalid due to not consistent");	
+					break;		
+				}
 				
+				const numDistance = Math.abs(parseInt(reportList[j]) - parseInt(reportList[j+1]))
+				
+				if (numDistance > 3) {
+					validity = false;	
+					console.log("invalid due to num");	
+					console.log(numDistance);
+					break;
+				}
+			}
+			
+			if (validity) {
+				numOfSafeReports++;
+			}
 
+		} else {
+			// decreasing order	
+			// inverse first signs	
+		
+			// valid until proven guilty
+			let validity = true;
 
+			for (let j = 0; j < reportList.length - 1; j++) {
+				if (parseInt(reportList[j]) < parseInt(reportList[j+1])) {
+					validity = false;
+					console.log("invalid due to not consistent");	
+					break;		
+				}
+				
+				const numDistance = Math.abs(parseInt(reportList[j]) - parseInt(reportList[j+1]))
+				
+				if (numDistance > 3) {
+					validity = false;	
+					console.log("invalid due to num");	
+					console.log(numDistance);
+					break;
+				}
+			}
+			
+			if (validity) {
+				numOfSafeReports++;
+			}
 
 		}
 
@@ -26,7 +81,6 @@ function solution() {
 
 }
 
-solution()
-
+console.log(solution());
 
 
